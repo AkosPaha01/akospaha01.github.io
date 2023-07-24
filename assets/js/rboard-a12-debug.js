2e2011f5bbdbb17aaa4bb9be45c7bde273ce2a48
PahaAkos <paha.akos@gmail.com>
2023-07-24 16:06:22 +0200

    Update Version Text

--- assets/js/rboard-a12-debug.js
+++ assets/js/rboard-a12-debug.js
@@ -1,4 +1,4 @@
-$.getJSON('https://github.com/DerTyp7214/RboardThemeManagerV3/releases/download/latest-debug/output-metadata.json', function(data) {
+$.getJSON('/debug-output-metadata.json', function(data) {
         var content = data.elements[0].versionName;
         $("#rboardversion-a12-debug").append(content);
    });
--- assets/js/rboard-a12-release.js
+++ assets/js/rboard-a12-release.js
@@ -1,4 +1,4 @@
-$.getJSON('https://github.com/DerTyp7214/RboardThemeManagerV3/releases/download/latest-release/output-metadata.json', function(data) {
+$.getJSON('/release-output-metadata.json', function(data) {
         var content = data.elements[0].versionName;
         $("#rboardversion-a12-release").append(content);
    });
